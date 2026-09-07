7ccef5b93f6a5e3e472fb005d33dac3afabb1d68
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-07 18:15:29 +0530

    Add accounting rollback migration

---
+++ migrations/000011_accounting_controls.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS property_expense_reversals;
+DROP TABLE IF EXISTS property_expenses;
+DROP TABLE IF EXISTS security_deposit_transactions;
+DROP TABLE IF EXISTS security_deposit_accounts;
+DROP TABLE IF EXISTS payment_reversals;
+DROP TABLE IF EXISTS rent_adjustments;
