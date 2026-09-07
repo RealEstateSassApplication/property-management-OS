2d14de4c7fd36b99d867bfa552acecc75d3ce3bd
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-07 18:11:03 +0530

    Create settings for newly added organizations

--- migrations/000010_organization_settings.up.sql
+++ migrations/000010_organization_settings.up.sql
@@ -8,6 +8,23 @@ CREATE TABLE organization_settings (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
+CREATE FUNCTION create_default_organization_settings()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO organization_settings (organization_id)
+    VALUES (NEW.id)
+    ON CONFLICT (organization_id) DO NOTHING;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER organizations_create_default_settings
+AFTER INSERT ON organizations
+FOR EACH ROW
+EXECUTE FUNCTION create_default_organization_settings();
+
 INSERT INTO organization_settings (organization_id)
 SELECT id FROM organizations
 ON CONFLICT (organization_id) DO NOTHING;
