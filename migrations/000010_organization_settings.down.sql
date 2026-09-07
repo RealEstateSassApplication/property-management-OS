b8fcb970f58acad25fec90f18eb7f3624da488ec
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-07 18:11:12 +0530

    Roll back organization settings trigger safely

--- migrations/000010_organization_settings.down.sql
+++ migrations/000010_organization_settings.down.sql
@@ -1,1 +1,3 @@
+DROP TRIGGER IF EXISTS organizations_create_default_settings ON organizations;
+DROP FUNCTION IF EXISTS create_default_organization_settings();
 DROP TABLE IF EXISTS organization_settings;
